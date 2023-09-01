1905a30c641a2032965dc6aba845f776406311d6
Tayllana Wislla <tayllanawislla@gmail.com>
2023-09-01 18:36:09 -0300

    Update update.php

--- api/update.php
+++ api/update.php
@@ -3,10 +3,10 @@ require('../config.php');
 
 if(strtolower($_SERVER['REQUEST_METHOD']) === 'put'){
     parse_str(file_get_contents('php://input'), $input);
-    $id = $input['id']??null;
-    $titulo = $input['titulo']??null;
-    $body = $input['body']??null;
-    
+    $id = filter_var($input['id']??null);
+    $titulo = filter_var($input['titulo']??null);
+    $body = filter_var($input['body']??null);
+
     if($titulo && $body && $id){
         $sql = $pdo->prepare("UPDATE notes SET titulo = :titulo, body = :body WHERE id = :id");
         $sql->bindValue(':id', $id);
@@ -19,7 +19,7 @@ if(strtolower($_SERVER['REQUEST_METHOD']) === 'put'){
         $array['error'] = 'Ooopss... Valores não enviados!';
     }
 }else{
-    $array['error'] = 'Ooopss... Método não permitido! (Somente POST)';
+    $array['error'] = 'Ooopss... Método não permitido! (Somente PUT)';
 }
 
 require('../return.php');
